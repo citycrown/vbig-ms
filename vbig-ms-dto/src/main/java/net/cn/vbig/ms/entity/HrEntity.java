36ca3ccf29cdd8467ce12670ca457f45b063caa0
陈冕 <chenmian@rainbowcn.com>
2019-05-24 15:24:14 +0800

    first commit

---
+++ vbig-ms-dto/src/main/java/net/cn/vbig/ms/entity/HrEntity.java
@@ -0,0 +1,145 @@
+/*
+ * 天虹商场股份有限公司所有.
+ */
+package net.cn.vbig.ms.entity;
+
+import com.sun.corba.se.pept.transport.ListenerThread;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 功能说明
+ *
+ * @author 陈冕
+ * @version 1.0.0
+ * @date 2019/5/24
+ */
+public class HrEntity implements UserDetails {
+    private Long id;
+    private String name;
+    private String phone;
+    private String telephone;
+    private String address;
+    private boolean enabled;
+    private String username;
+    private String password;
+    private String remark;
+    //private List<Role> roles;
+    private String userface;
+    private Integer status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    // 账户是否未锁定
+    @Override
+    public boolean isAccountNonLocked() {
+        return getStatus() != Status.LOCKED;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getUserface() {
+        return userface;
+    }
+
+    public void setUserface(String userface) {
+        this.userface = userface;
+    }
+    
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    public interface Status {
+        int DISABLED = 0;
+        int VALID = 1;
+        int LOCKED = 2;
+    }
+}
